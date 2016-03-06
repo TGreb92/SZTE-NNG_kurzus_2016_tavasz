5b850a3fb92c342956d3beaf003880e89908470d
Gábor <grebottz@gmail.com>
2016-03-06 11:30:59 +0100

    changed tabs and fixed magic number

--- src/main/java/challenge/Unicorn.java
+++ src/main/java/challenge/Unicorn.java
@@ -1,10 +1,11 @@
 package challenge;
 
 public class Unicorn {
-	private static int previousLineNumber = -1;
+    private static int previousLineNumber = -1;
+    private final static int LOGGER_METHOD_CALL_LOCATION = 2;
 
 	public static boolean pat() {
-		int lineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
+	    int lineNumber = Thread.currentThread().getStackTrace()[LOGGER_METHOD_CALL_LOCATION].getLineNumber();
         boolean isNewLineNumber = lineNumber != previousLineNumber;
         previousLineNumber = lineNumber;
         return isNewLineNumber;
